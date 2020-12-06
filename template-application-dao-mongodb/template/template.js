db1095da4ea6a23c6748e048d6039ad9890a2a26
delchev <delchevn@gmail.com>
2020-12-06 23:17:22 +0200

    initial

---
+++ template-application-dao-mongodb/template/template.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2010-2020 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *   SAP - initial API and implementation
+ */
+
+exports.getTemplate = function (parameters) {
+    return {
+        name: "Application - DAO - MongoDB",
+        description: "Application with DAO for MongoDB",
+        extension: "model",
+        sources: [{
+            location: "/template-application-dao-mongodb/data/dao/entity.js.template",
+            action: "generate",
+            rename: "data/dao/{{perspectiveName}}/{{fileName}}.js",
+            engine: "velocity",
+            collection: "models"
+        }, {
+            location: "/template-application-dao-mongodb/data/utils/EntityUtils.js.template",
+            action: "copy",
+            rename: "data/utils/EntityUtils.js"
+        }],
+        parameters: []
+    };
+};
